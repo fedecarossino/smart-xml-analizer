862a7e363c77f0773d18e2bfa64d189a06025af5
Federico Carossino <>
2018-07-21 03:07:14 -0300

    little refactor

--- src/main/java/com/test/services/analyzer/strategy/IdMatchStrategies.java
+++ src/main/java/com/test/services/analyzer/strategy/IdMatchStrategies.java
@@ -5,10 +5,13 @@ import org.jsoup.nodes.Element;
 import java.util.List;
 
 public class IdMatchStrategies implements MatchStrategies {
+
+    public static final String ELEMENT_ID = "id";
+
     @Override
     public int match(Element original, Element diffCaseElement, List<String> matches) {
         if(original.id().equals(diffCaseElement.id())){
-            matches.add("id");
+            matches.add(ELEMENT_ID);
             return 100;
         }
         return 0;
--- src/main/java/com/test/services/analyzer/strategy/TextMatchStrategies.java
+++ src/main/java/com/test/services/analyzer/strategy/TextMatchStrategies.java
@@ -5,11 +5,14 @@ import org.jsoup.nodes.Element;
 import java.util.List;
 
 public class TextMatchStrategies implements MatchStrategies {
+
+    public static final String ELEMENT_TEXT = "Text";
+
     @Override
     public int match(Element original, Element diffCaseElement, List<String> matches) {
         if(original.hasText() && diffCaseElement.hasText()
                 && original.text().equals(diffCaseElement.text())){
-            matches.add("Text");
+            matches.add(ELEMENT_TEXT);
             return 25;
         }
 
